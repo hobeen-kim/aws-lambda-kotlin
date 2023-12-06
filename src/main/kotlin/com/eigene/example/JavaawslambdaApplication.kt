6b114b103ba743e271638e743c05c90a208e37eb
hobenn-kim <sksjsksh32@naver.com>
2023-12-06 21:25:18 +0900

    first commit

---
+++ src/main/kotlin/com/eigene/example/JavaawslambdaApplication.kt
@@ -0,0 +1,11 @@
+package com.eigene.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaawslambdaApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaawslambdaApplication>(*args)
+}
---
+++ src/test/kotlin/com/eigene/example/JavaawslambdaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eigene.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavaawslambdaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
